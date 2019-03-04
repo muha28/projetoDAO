faa88fb1839d12723ed0c862f02b4c5a27142859
Alisson Muhammad <alisson.muhammad@hotmail.com>
2019-03-04 08:57:17 -0300

    criacao do metodo update

--- projectDAO/src/application/Program.java
+++ projectDAO/src/application/Program.java
@@ -33,10 +33,17 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== Test 4: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		System.out.println("\n=== Test 4: seller insert ===");
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Wayne");
+		seller.setEmail("mw@wayneenterprise.com");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 	}
 
 }
